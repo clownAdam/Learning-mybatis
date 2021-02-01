484ce174d1fae54504d23b135d277558dd329e9d
zkr_chensiqi <3406869297@qq.com>
2021-02-01 15:52:10 +0800

    mybatis tradition dao develop

---
+++ power-node/src/main/java/cn/clown/dao/impl/StudentDaoImpl.java
@@ -0,0 +1,33 @@
+package cn.clown.dao.impl;
+
+import cn.clown.common.MyBatisUtil;
+import cn.clown.dao.StudentDao;
+import cn.clown.domain.Student;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+/**
+ * dao接口实现类
+ *
+ * @author clown
+ * @date 2021
+ */
+public class StudentDaoImpl implements StudentDao {
+    @Override
+    public List<Student> selectAllStudents() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        List<Student> selectAllStudents = sqlSession.selectList("selectAllStudents");
+        sqlSession.close();
+        return selectAllStudents;
+    }
+
+    @Override
+    public int insertStudent(Student student) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        int flag = sqlSession.insert("insertStudent", student);
+        sqlSession.commit();
+        sqlSession.close();
+        return flag;
+    }
+}
